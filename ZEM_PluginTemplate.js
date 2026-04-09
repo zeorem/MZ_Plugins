c6bd7dcc72f306630b967b007bdb82c60df53994
drgrantx <ricardocassola94@tutamail.com>
2026-04-09 08:58:30 -0600

    Add MZ Plugin Template

---
+++ ZEM_PluginTemplate.js
@@ -0,0 +1,19 @@
+//=============================================================================
+// Plugin Template
+// ZEM_PluginTemplate.js
+//=============================================================================
+
+/*:
+ * @plugindesc Plugin that does stuff
+ * @author Zeorem
+ * @url https://itch.io/zeorem
+ * @version 0.1
+ * @target MZ
+ * @help
+ * Help text
+ * 
+ */
+
+(() => {
+    console.log("I am therefore am coded");
+})();    
